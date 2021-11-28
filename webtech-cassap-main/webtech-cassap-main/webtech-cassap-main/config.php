f77760529d6219683f55e103b46f0bcc36115d7b
Silas Sangmin <sangminsilas@gmail.com>
2021-11-28 12:44:08 +0000

    All files addedch

--- webtech-cassap-main/webtech-cassap-main/webtech-cassap-main/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define("servername", "localhost:3307");
-    define("username", "root");
-    define("password", "");
-    define("database", "lab_post");
-
-    $connection = mysqli_connect(servername, username, password, database);
-?>
--- webtech-cassap-main/webtech-cassap-main/webtech-cassap-main/config/database_credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-# Database connection variables
-    define("SERVERNAME", "localhost");
-    define("USERNAME", "root");
-    define("PASSWORD", "");
-    define("DATABASE", "cass_app");
-?>
